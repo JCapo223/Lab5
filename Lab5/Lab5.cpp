b6b3aea53b470de5747a8cbc3eae9ff84e03db2e
Joseph Domenic Capo <jcapo@purdue.edu>
2022-09-28 11:51:33 -0400

    Add project files.

---
+++ Lab5/Lab5.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <string> 
+using namespace std;
+
+int main()
+{
+    vector<char> intVector({ 'V','F','X','B','L','I','T','Z','J','R','P','H','D','K','N','O','W','S','G','U','Y','Q','M','A','C','E' });
+    intVector[2];
+
+    std::string(text);
+    getline(cin, text);
+
+    for (int I = 0; I < text.length(); I++) {
+        text[I];
+
+        if (text[I] >= 65 && text[I] <= 90)
+            cout << intVector[text[I] - 65];
+        else if(text[I] >= 97 && text[I] <= 122);
+            cout << intVector[text[I] - 32];
+
+
+    }
+
+}
